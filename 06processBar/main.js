11fd0be64cc7d75829050890a20174d4335c4c14
Johnson Xin <2775145275@qq.com>
2023-07-10 15:46:51 +0800

    processBar

---
+++ 06processBar/main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require("electron");
+
+let progressInterval;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+
+  const INCREMENT = 0.03;
+  const INTERVAL_DELAY = 100; // ms
+
+  let c = 0;
+  progressInterval = setInterval(() => {
+    win.setProgressBar(c);
+
+    if (c < 2) {
+      c += INCREMENT;
+    } else {
+      c = -INCREMENT * 5;
+    }
+  }, INTERVAL_DELAY);
+}
+
+app.whenReady().then(createWindow);
+
+app.on("before-quit", () => {
+  clearInterval(progressInterval);
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWin();
+  }
+});
